ec3b1e5930e95a003d22933651b716b35972bfdb
Martin <martin.andrzejak1@gmail.com>
2021-02-09 10:33:31 +0100

    ggplots

--- brms_code_pollination.R
+++ brms_code_pollination.R
@@ -8,7 +8,6 @@ library(ggplot2)
 library(brms)
 library(xlsx)
 
-
 ## Note: inorder to run the models you need the parallel package, since I use the detectCores function
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -16,11 +15,18 @@ library(xlsx)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 dia_dat <- read.csv("https://raw.githubusercontent.com/Martin19910130/Bayesian_pollination/main/Dia_seeds_per_capsule.csv")
 
+ggplot(dia_dat, aes(x = climate, y = Seeds_capsule, fill = treatment)) + geom_boxplot() + 
+  facet_wrap(~season) + theme_bw()
+
 dia_mod <- brm(Seeds_capsule ~ climate * treatment * season + (1|individual_id/plot_id/climate) + (1|plot_id/climate) +
                  ar(gr = individual_id), 
                cores = parallel::detectCores() - 1, iter = 4000, data = dia_dat)
 
 sca_dat <- read.csv("https://raw.githubusercontent.com/Martin19910130/Bayesian_pollination/main/Sca_seeds_per_flowerhead.csv")
+
+ggplot(sca_dat, aes(x = climate, y = seeds_flowerhead, fill = treatment)) + geom_boxplot() + 
+  facet_wrap(~ season) + theme_bw()
+
 
 sca_mod <-  brm(seeds_flowerhead ~ climate * treatment * season + 
                   (1|plot_id/climate) + (1|individual_id/plot_id/climate) + ar(gr = individual_id), 
